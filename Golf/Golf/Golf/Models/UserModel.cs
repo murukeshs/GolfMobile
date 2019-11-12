6cb2cb29585d00b24120d0c7e4fabdf2f7bf4cdc
swathithayalan <Swathithayalan@123>
2019-11-12 16:26:52 +0530

    login api by swa

--- Golf/Golf/Golf/Models/UserModel.cs
+++ Golf/Golf/Golf/Models/UserModel.cs
@@ -8,5 +8,11 @@ namespace Golf.Models
     {
         internal string BaseUrl = "https://golfapp.azurewebsites.net/api/";
         internal string AccessToken { get; set; } = string.Empty;
+
+        internal string UserName { get; set; } = string.Empty;
+
+        internal int UserId { get; set; } = 0;
+
+        internal string UserEmail { get; set; } = string.Empty;
     }
 }
