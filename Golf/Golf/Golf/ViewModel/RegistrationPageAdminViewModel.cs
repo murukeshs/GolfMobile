4a9d442340e8dd173907883df035949abf91498d
swathithayalan <Swathithayalan@123>
2019-11-21 22:18:06 +0530

    issue fix

--- Golf/Golf/Golf/ViewModel/RegistrationPageAdminViewModel.cs
+++ Golf/Golf/Golf/ViewModel/RegistrationPageAdminViewModel.cs
@@ -9,7 +9,7 @@ using Plugin.Connectivity;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Threading.Tasks;
+using System.Threading.Tasks; 
 using System.Windows.Input;
 using Xamarin.Forms;
 
