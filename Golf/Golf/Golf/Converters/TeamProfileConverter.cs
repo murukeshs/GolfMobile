768df1929c0812f1a17393ce3253358668e7e31a
santhoshramesh <santhoshr@apptomate.co>
2019-11-21 21:16:17 +0530

    Code Updated.

--- Golf/Golf/Golf/Converters/TeamProfileConverter.cs
+++ Golf/Golf/Golf/Converters/TeamProfileConverter.cs
@@ -21,7 +21,13 @@ namespace Golf.Converters
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if ((string)value == string.Empty || (string)value == null || (string)value == "")
+                return "profile_defalut_pic.png"; //Paymet is success
+            else
+            {
+                var image = (string)value;
+                return image;//Paymet is failed
+            }
         }
     }
 }
