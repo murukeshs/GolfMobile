4e6fc72bb09dd7c0c53852a1232dc48fdfef6496
Swathi Thayalakrishnan <swathit@apptomate.co>
2019-12-17 12:17:47 +0530

    issue fix

--- Golf/Golf/Golf/ViewModel/InviteParticipantPageViewModel.cs
+++ Golf/Golf/Golf/ViewModel/InviteParticipantPageViewModel.cs
@@ -183,6 +183,7 @@ namespace Golf.ViewModel
                         phoneNumber = PhoneNumber,
                         isEmailNotification = IsEmailNotification,
                         isSMSNotification = IsSMSNotification,
+                        userTypeId = "1"
                     };
 
                     string json = JsonConvert.SerializeObject(data);
